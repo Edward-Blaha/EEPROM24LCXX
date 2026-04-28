06838fe8898bc3814eb836a02f106fac2a9cff45
Edward Blaha <69668372+Edward-Blaha@users.noreply.github.com>
2026-04-28 17:58:19 +1000

    Reading and writing a string with lib
    This example demonstrates writing a string to and reading bytes from a 24LCxx EEPROM module

---
+++ examples/ReadWriteString/ReadWriteString.ino
@@ -0,0 +1,37 @@
+/*-----------------------------------------------------------------------------*/
+// Project: 24LCXX EEPROM controller lib                                       //
+// Description: Example code for writing a string to and reading bytes         //
+// from an external 24LCxx family EEPROM using a custom library.               //
+// (Code tested with 24LC16)                                                   //
+// Author: Edward Blaha                                                        //
+// Date: April 2026                                                            //
+/*-----------------------------------------------------------------------------*/
+
+#include <EEPROM24LCXX.h> // custom lib
+#include <Arduino.h>
+#include <Wire.h>
+
+EEPROM24LCXX eeprom;
+char* dataMain = "(LIBRARY TO WRITE TO AN EEPROM UTILISING I2C (designed to work with the 24LCxx family)";
+int sizeOfData;
+char data[512]; // 512 byte buffer, Change for max amount of data you plan on running within your system
+void setup() {
+  eeprom.begin();
+  delay(50);
+  Serial.begin(9600); 
+  // Write data using lib
+  eeprom.writeStringToEEPROM(0,dataMain); // 0 = starter EEPROM address, dataMain = message
+}
+
+void loop() {
+  sizeOfData = strlen(dataMain);
+  for(int i = 0; i<sizeOfData; i++){ // read Bytes from eeprom, and assign to array
+    data[i] = eeprom.ReadByte(i);
+  }
+  Serial.print("Data Recieved: ");
+  Serial.println();
+  for(int i = 0; i<sizeOfData; i++){
+    Serial.print(data[i]);
+  }
+  Serial.println("\n");
+}
